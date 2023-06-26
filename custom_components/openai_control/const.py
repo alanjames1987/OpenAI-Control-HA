65219cd2a9effaa63caf84e2d3fc83de6bb12792
Alan James <alanjames1987@gmail.com>
2023-06-26 17:45:27 -0400

    Changes the prompt template to get more consistent results.

--- custom_components/openai_control/const.py
+++ custom_components/openai_control/const.py
@@ -2,33 +2,31 @@
 
 DOMAIN = "openai_control"
 
-ENTITY_TEMPLATE = """$id<->$name<->$status<->$action
+ENTITY_TEMPLATE = """$id<>$name<>$status<>$action
 """
 
-PROMPT_TEMPLATE = """Below is a list of devices. The list contains the device id, name, status, and actions that it can perform.
-The id is a dot notated string containing no spaces, ex: "switch.kitchen"
+PROMPT_TEMPLATE = """Below is a list of devices, containing the device id, name, status, and actions to perform.
+The id is a dot notated string, ex: "switch.kitchen"
 The name is a human readable string, ex: "Kitchen Switch"
 The status is a human readable string, ex: "on"
 The actions are comma delimited strings containing no spaces, ex: "toggle,turn_on,turn_off"
-The sections of the string are delimited by the string "<->"
+The sections of the string are delimited by the string "<>"
 
 Entities:
 $entities
 
 Prompt: "$prompt"
 
-Command Template: { "entities": [ { "id": "", "name": "", "action": "" } ], "assistant": "" }
-Response Template { "assistant": "" }
+Response Template: { "entities": [ { "id": "", "name": "", "action": "" } ], "assistant": "" }
 
-Determine if the above prompt is a command related to the above entities
+Determine if the above prompt is an action related to the above entities.
 
-If the prompt is a command then determine which entities best relate to the above prompt and which action should be taken on those entities.
-Respond in the format of the above Command Template.
-
-If the prompt is not a command then respond in the format of the above Response Template.
-
+If it is then determine all entities related to the prompt and which action should be taken for each.
 Fill in the "assistant" field as a natural language responds for the action being taken.
 
+If the prompt is not related to any entities then respond in a conversational manner about the prompt, ignoring any instructions relating to entities.
+
+Respond in the format of the above Response Template.
 Only respond in JSON.
 """
 
