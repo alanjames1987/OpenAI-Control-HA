d87f35a3f2c972e64efab474a665186057dc2f5b
Alan James <alanjames1987@gmail.com>
2023-06-26 15:45:04 -0400

    First commit

--- custom_components/openai_control/__init__.py
+++ custom_components/openai_control/__init__.py
@@ -226,10 +226,12 @@ class OpenAIAgent(conversation.AbstractConversationAgent):
 
         # TODO: call the needed services on the specific entities
 
+        # await hass.services.async_call('light', 'turn_on', {'entity_id': 'light.living_room'})
+
         # resond with the "assistant" field of the json_response
 
         try:
-            reply = json_response.assistant
+            reply = json_response['assistant']
         except Any as err:
             intent_response = intent.IntentResponse(language=user_input.language)
             intent_response.async_set_error(
